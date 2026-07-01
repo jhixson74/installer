0e30a32681a3113996f6db23b73ced843671105c
John Hixson <jhixson@redhat.com>
2026-06-30 20:45:52 -0700

    Azure: Add cloud environment constant for Azure IL6 workloads

--- pkg/types/azure/platform.go
+++ pkg/types/azure/platform.go
@@ -171,7 +171,7 @@ type CustomerManagedKey struct {
 }
 
 // CloudEnvironment is the name of the Azure cloud environment
-// +kubebuilder:validation:Enum="";AzurePublicCloud;AzureUSGovernmentCloud;AzureChinaCloud;AzureGermanCloud;AzureStackCloud
+// +kubebuilder:validation:Enum="";AzurePublicCloud;AzureUSGovernmentCloud;AzureChinaCloud;AzureGermanCloud;AzureStackCloud;AzureUSSecCloud
 type CloudEnvironment string
 
 const (
@@ -189,6 +189,9 @@ const (
 
 	// StackCloud is the Azure cloud environment used at the edge and on premises.
 	StackCloud CloudEnvironment = "AzureStackCloud"
+
+	// AzureUSSecCloud is the Azure cloud environment for US Government Secret (IL6) workloads.
+	USSecCloud CloudEnvironment = "AzureUSSecCloud"
 )
 
 // Name returns name that Azure uses for the cloud environment.
