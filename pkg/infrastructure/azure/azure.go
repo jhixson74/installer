fa83f53556da6ed2c3de672632d8088ed5f9d3e9
John Hixson <jhixson@redhat.com>
2026-07-10 12:54:15 -0700

    pkg/infrastructure/azure: use endpoint from storage account

--- pkg/infrastructure/azure/azure.go
+++ pkg/infrastructure/azure/azure.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"net/url"
 	"strings"
 	"time"
 
@@ -256,9 +257,6 @@ func (p *Provider) InfraReady(ctx context.Context, in clusterapi.InfraReadyInput
 	containerName := "vhd"
 	blobName := fmt.Sprintf("rhcos%s.vhd", randomString(5))
 
-	storageURL := fmt.Sprintf("https://%s.blob.%s", storageAccountName, session.Environment.StorageEndpointSuffix)
-	blobURL := fmt.Sprintf("%s/%s/%s", storageURL, containerName, blobName)
-
 	var storageAccount *armstorage.Account
 	var storageClientFactory *armstorage.ClientFactory
 	var storageAccountKeys []armstorage.AccountKey
@@ -292,6 +290,12 @@ func (p *Provider) InfraReady(ctx context.Context, in clusterapi.InfraReadyInput
 		storageAccountKeys = createStorageAccountOutput.StorageAccountKeys
 
 		logrus.Debugf("StorageAccount.ID=%s", *storageAccount.ID)
+	}
+
+	storageURL := *storageAccount.Properties.PrimaryEndpoints.Blob
+	blobURL, err := url.JoinPath(storageURL, containerName, blobName)
+	if err != nil {
+		return err
 	}
 
 	// Create a managed image, which is used for OKD or confidential VMs on OCP.
